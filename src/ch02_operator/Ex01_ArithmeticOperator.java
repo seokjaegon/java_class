406d5eb6ea6b87bb761e1f85f39ef4332238a760
seokjaegon <seokjageon@naver.com>
2023-11-30 15:53:40 +0900

    연산자

---
+++ src/ch02_operator/Ex01_ArithmeticOperator.java
@@ -0,0 +1,22 @@
+package ch02_operator;
+
+public class Ex01_ArithmeticOperator {
+
+	public static void main(String[] args) {
+		//산술연산자
+		/**
+		* num1, num2 두개읠 정수형 변수를 선언하고 값은 마음대로
+		*두 변수의 +, -, *,/, % 연산결과를 print() 를 활용하여 출력해봅시다.
+		*출력방법
+		* System.out.println(num1+num2)
+		 */
+		int num1 = 10;
+		int num2 = 6;
+		System.out.println(num1+num2);
+		System.out.println(num1-num2);
+		System.out.println(num1*num2);
+		System.out.println(num1/num2);
+		System.out.println(num1%num2);
+	}
+
+}
