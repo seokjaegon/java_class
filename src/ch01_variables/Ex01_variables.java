90fcb500cbecd144b0fe4a059f541526f9e85ecc
seokjaegon <seokjageon@naver.com>
2023-11-30 13:28:55 +0900

    변수 추가

---
+++ src/ch01_variables/Ex01_variables.java
@@ -0,0 +1,15 @@
+package ch01_variables;
+
+public class Ex01_variables {
+
+	public static void main(String[] args) {
+		// 본인의 이름(name), 나이(age), 키(height), 주소(address), 전화번호(mobile)
+		// 를 입력할 수 있는 변수를 각각 선언해봅시다.
+		String name = "석재곤";
+		int age = 25;
+		double height = 175;
+		String address = "인천시 미추홀구 도화동";
+		String mobile = "010-9018-8437";
+	}
+
+}
