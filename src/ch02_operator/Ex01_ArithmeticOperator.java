65083fbfef5d991ef0148b807102d782b2b75953
seokjaegon <seokjageon@naver.com>
2023-12-01 09:01:13 +0900

    비교연산

--- src/ch02_operator/Ex01_ArithmeticOperator.java
+++ src/ch02_operator/Ex01_ArithmeticOperator.java
@@ -10,13 +10,37 @@ public class Ex01_ArithmeticOperator {
 		*출력방법
 		* System.out.println(num1+num2)
 		 */
-		int num1 = 10;
-		int num2 = 6;
-		System.out.println(num1+num2);
+		int num1 = 10, num2 = 6;
+		System.out.println("덧셈 결과 : "+(num1+num2));
 		System.out.println(num1-num2);
 		System.out.println(num1*num2);
 		System.out.println(num1/num2);
 		System.out.println(num1%num2);
+		
+		//계산 결과를 담을 변수 활용
+		int result = 0;
+		result = num1 + num2;
+		System.out.println(result);
+		result = num1 - num2;
+		System.out.println(result);
+		result = num1 * num2;
+		System.out.println(result);
+		result = num1 / num2;
+		System.out.println(result);
+		result = num1 % num2;
+		System.out.println(result);
+		
+		//계산 결과 변수를 따로 선언
+		int sumResult = num1 + num2;
+		System.out.println(sumResult);
+		int subResult = num1 - num2;
+		System.out.println(subResult);
+		int mulResult = num1 * num2;
+		System.out.println(mulResult);
+		int divResult = num1 / num2;
+		System.out.println(divResult);
+		int remainResult = num1 % num2;
+		System.out.println(remainResult);
 	}
 
 }
