07ccb3c0e58b1c5e04af1998732ca2a61f92a7b0
seokjaegon <seokjageon@naver.com>
2023-12-06 16:20:27 +0900

    while문 예제(음수 양수 합계)

---
+++ src/ch07_while/Ex07_WhileEx.java
@@ -0,0 +1,27 @@
+package ch07_while;
+
+public class Ex07_WhileEx {
+    public static void main(String[] args) {
+        /**
+         * 1 + (-2) + 3 + (-4) + 5 + (-6) ~~~~
+         * 처럼 계산을 했을 때 합계가 100이 됐을 때의 숫자를 출력
+         * 정답 : 199
+         */
+        // 합계용 변수
+        int sum = 0;
+        // 1, -2, 3, -4, 5, -6~~
+        int i = 0;
+        while (true) {
+            i++;
+            if(i % 2 == 0) {
+                sum -= i;
+            } else {
+                sum += i;
+            }
+            if (sum == 100) {
+                System.out.println("정답 : " + i);
+                break;
+            }
+        }
+    }
+}
