503142e9c6f7550b38018e82da038a276ced5e4b
seokjaegon <seokjageon@naver.com>
2023-12-12 09:55:18 +0900

    객체지향 시작

---
+++ src/ch10_class/ex2/Student.java
@@ -0,0 +1,13 @@
+package ch10_class.ex2;
+
+public class Student {
+    // 필드선언
+    String studentName;
+    String studentMajor;
+    String studentMobile;
+
+    // 기본생성자
+    public Student() {
+        System.out.println("Student 기본생성자");
+    }
+}
