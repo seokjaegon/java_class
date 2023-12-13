a436e97f5229f437263d837d25b4b3a8faa34478
seokjaegon <seokjageon@naver.com>
2023-12-13 16:31:16 +0900

    객체지향-클래스활용

---
+++ src/ch10_class/ex8/Book.java
@@ -0,0 +1,26 @@
+package ch10_class.ex8;
+
+public class Book {
+    private String bookTitle;
+    private String bookAuthor;
+
+    // bookTitle getter
+    public String getBookTitle() {
+        return bookTitle;
+    }
+
+    // bookTitle setter
+    public void setBookTitle(String bookTitle) {
+        this.bookTitle = bookTitle;
+    }
+
+    // bookAuthor getter
+    public String getBookAuthor() {
+        return bookAuthor;
+    }
+
+    // bookAuthor setter
+    public void setBookAuthor(String bookAuthor) {
+        this.bookAuthor = bookAuthor;
+    }
+}
