1067cff4c6234b236ceefb955e3c0a8e480c67eb
seokjaegon <seokjaegon@naver.com>
2023-12-22 14:19:08 +0900

    MainController 주석

--- src/ch11_classes/ex06_memberboard/MainController.java
+++ src/ch11_classes/ex06_memberboard/MainController.java
@@ -17,18 +17,25 @@ public class MainController {
             System.out.print("선택> ");
             int select = scanner.nextInt();
             if (select == 1) {
+                // 회원 가입 기능 호출 문장
                 memberService.save();
             } else if (select == 2) {
+                // 회원 로그인 기능 호출 문장
                 memberService.login();
             } else if (select == 3) {
+                // 회원목록 기능 호출 문장
                 memberService.findAll();
             } else if (select == 4) {
+                // 회원수정 기능 호출 문장
                 memberService.update();
             } else if (select == 5) {
+                // 회원탈퇴 기능 호출 문장
                 memberService.delete();
             } else if (select == 6) {
+                // 회원 로그아웃 기능 호출 문장
                 memberService.logout();
             } else if (select == 7) {
+                // 회원 게시글 기능 호출 문장
                 memberService.board();
             } else if (select == 0) {
                 break;
