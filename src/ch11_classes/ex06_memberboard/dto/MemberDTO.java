a4330af452c792c75412f4baa3af360750066d67
seokjaegon <seokjaegon@naver.com>
2023-12-22 13:52:03 +0900

    MemberDTO 주석

--- src/ch11_classes/ex06_memberboard/dto/MemberDTO.java
+++ src/ch11_classes/ex06_memberboard/dto/MemberDTO.java
@@ -4,13 +4,22 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class MemberDTO {
+    /**
+     * 필드 선언
+     *      회원 관리번호(id)
+     *      회원 이메일(memberEmail)
+     *      회원 비밀번호(memberPassword)
+     *      회원 이름(memberName)
+     *      회원 전화번호(memberMobile)
+     *      회원 등록날짜(createdAt)
+     */
     private Long id;
     private String memberEmail;
     private String memberPassword;
     private String memberName;
     private String memberMobile;
     private String createdAt;
-
+    // 모든 필드의 getter/setter
     public Long getId() {
         return id;
     }
@@ -58,13 +67,13 @@ public class MemberDTO {
     public void setCreatedAt(String createdAt) {
         this.createdAt = createdAt;
     }
-
+    // MemberDTO의 기본생성자
     public MemberDTO() {
 
     }
 
     private static Long idValue = 1L;
-
+    // MemberDTO의 매개변수가 있는 기본생성자
     public MemberDTO(String memberEmail, String memberPassword, String memberName, String memberMobile) {
         this.id = idValue++;
         this.memberEmail = memberEmail;
