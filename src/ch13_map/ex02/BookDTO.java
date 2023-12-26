3d3ab76292312397fbf9e5e8b1502ddc2b835af7
seokjaegon <seokjaegon@naver.com>
2023-12-26 10:57:55 +0900

    Book 예제에 map 적용 시작

---
+++ src/ch13_map/ex02/BookDTO.java
@@ -0,0 +1,75 @@
+package ch13_map.ex02;
+
+public class BookDTO {
+    private Long id;
+    private String bookTitle;
+    private String bookAuthor;
+    private int bookPrice;
+    private String bookPublisher;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBookTitle() {
+        return bookTitle;
+    }
+
+    public void setBookTitle(String bookTitle) {
+        this.bookTitle = bookTitle;
+    }
+
+    public String getBookAuthor() {
+        return bookAuthor;
+    }
+
+    public void setBookAuthor(String bookAuthor) {
+        this.bookAuthor = bookAuthor;
+    }
+
+    public int getBookPrice() {
+        return bookPrice;
+    }
+
+    public void setBookPrice(int bookPrice) {
+        this.bookPrice = bookPrice;
+    }
+
+    public String getBookPublisher() {
+        return bookPublisher;
+    }
+
+    public void setBookPublisher(String bookPublisher) {
+        this.bookPublisher = bookPublisher;
+    }
+
+    public BookDTO() {
+
+    }
+
+    private static Long idValue = 1L;
+
+    public BookDTO(String bookTitle, String bookAuthor, int bookPrice, String bookPublisher) {
+        this.id = idValue++;
+        this.bookTitle = bookTitle;
+        this.bookAuthor = bookAuthor;
+        this.bookPrice = bookPrice;
+        this.bookPublisher = bookPublisher;
+    }
+
+    @Override
+    public String toString() {
+        return "BookDTO{" +
+                "id=" + id +
+                ", bookTitle='" + bookTitle + '\'' +
+                ", bookAuthor='" + bookAuthor + '\'' +
+                ", bookPrice=" + bookPrice +
+                ", bookPublisher='" + bookPublisher + '\'' +
+                '}';
+    }
+
+}
